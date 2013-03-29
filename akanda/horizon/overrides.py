fa1b31785c52f0e4a14ed57663a3904d0ecd976d
Rosario Di Somma <rosario.disomma@dreamhost.com>
2013-03-29 20:35:28 +0100

    Move the Akanda Networking panel in the new Manage Network section
    In the Grizzly version of Horizon the navigation tree on the left
    is been split in two section, this fix move the Akanda Networking
    panel from the Manage Compute sections to the newly created
    Manage Network section.
    
    Change-Id: Iea6156bcc85496e3efac8c2bf4c50543dfb11a3d
    Signed-off-by: Rosario Di Somma <rosario.disomma@dreamhost.com>

--- akanda/horizon/overrides.py
+++ akanda/horizon/overrides.py
@@ -2,5 +2,5 @@ from horizon.base import Horizon
 
 
 nova_dashboard = Horizon.get_dashboard('project')
-compute_panel_group = nova_dashboard.get_panel_group('compute')
+compute_panel_group = nova_dashboard.get_panel_group('network')
 compute_panel_group.panels.append('networking')
