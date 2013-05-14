e9b70235dd578f8db0eafc0bfee582c71b717ea9
Rosario Di Somma <rosario.disomma@dreamhost.com>
2013-05-14 18:19:19 +0200

    Fix the public IP display in Horizon for routers
    DHC-1150
    
    Change-Id: Ie455db4243d54a6d95a9a97f9e044127eed67df8
    Signed-off-by: Rosario Di Somma <rosario.disomma@dreamhost.com>

--- akanda/horizon/configuration/tables/publicips.py
+++ akanda/horizon/configuration/tables/publicips.py
@@ -1,5 +1,6 @@
 import logging
 
+from django import template
 from django.utils.translation import ugettext as _
 
 from horizon import tables
@@ -9,9 +10,15 @@ LOG = logging.getLogger(__name__)
 LOG.debug('importing publicips table')
 
 
+def get_ips(router):
+    template_name = 'akanda/configuration/_router_ips.html'
+    context = {'router': router}
+    return template.loader.render_to_string(template_name, context)
+
+
 class PublicIPsTable(tables.DataTable):
     router_name = tables.Column('router_name', verbose_name=_("Router Name"))
-    ipaddr = tables.Column('ipaddr', verbose_name=_("IP Address"))
+    ipaddr = tables.Column(get_ips, verbose_name=_("IP Address"))
 
     class Meta:
         name = "publicips"
