7c7d40e69013beb30426040ce7f3c72378fc38da
Ryan Petrello <lists@ryanpetrello.com>
2013-09-20 16:48:55 -0400

    Override the router detail view to properly look up port data w/ akanda
    DHC-1455

--- akanda/horizon/overrides.py
+++ akanda/horizon/overrides.py
@@ -4,3 +4,11 @@ from horizon.base import Horizon
 nova_dashboard = Horizon.get_dashboard('project')
 compute_panel_group = nova_dashboard.get_panel_group('network')
 compute_panel_group.panels.append('networking')
+
+#
+# Manually override the definition of the router detail view method
+# to properly look up port data w/ akanda. May God have mercy on my soul.
+#
+from openstack_dashboard.dashboards.project.routers import views
+from akanda.horizon.routers.views import get_interfaces_data
+views.DetailView.get_interfaces_data = get_interfaces_data
